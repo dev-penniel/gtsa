48cf18b6377e82aa02bc789736833ca826e080c6
dev-penniel <tlhonolofatsopennielmothae@gmail.com>
2025-04-16 05:19:58 +0200

    Seeder bug fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,7 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([
-            RolePermissionSeeder::class,
+            PermissionSeeder::class,
             UserSeeder::class,
         ]);
     }

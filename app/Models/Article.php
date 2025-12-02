5085bd8a1793530edf284e6c82d0f164b5acdf2b
= <=>
2025-12-02 12:26:48 +0200

    Article model and migration

---
+++ app/Models/Article.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Spatie\Permission\Traits\HasRoles;
+
+class Article extends Model
+{
+
+    use HasRoles;
+    
+    protected $fillable = [
+        'title',
+        'slug',
+        'body',
+        'exerpt',
+        'views',
+        'claps',
+        'thumbnail',
+        'img_credit',
+        'status',
+        'published_date',
+    ];
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
+    }
+}
